f139737833cf10d8bdc84a703c6e02ce9ccf1ce5
Programadorcsl <caiosousalimaa@gmail.com>
2025-06-05 22:00:37 +0000

    realizei o desafio novato csl

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,41 @@
 //Teste larissa
 
 int main() {
+   printf("Desafio novato \n");
+
+    char nome[50] = "Bahia";
+    float habitantes = 500000;
+    double area = 1500 ;
+    char codigo = 1;
+    float pib = 699.28 ;
+    int numero = 50;
+
+    printf("Qual o nome do estado ?\n");
+    scanf("%s", &nome);
+
+    printf("Quantos habitantes possui?\n");
+    scanf("%f" , &habitantes);
+
+    printf(" Qual a área da cidade ?\n");
+    scanf("%f" , &area);
+
+    printf("Qual o código?\n");
+    scanf("%s" , &codigo);
+    
+    printf(" Quanto é o pib?\n");
+    scanf("%f" , &pib);
+    
+    printf("quantos pontos turísticos possui ?\n");
+    scanf("%d" , &numero);
+
+    printf("nome da cidade: %s\n" , nome);
+    printf("número de habitantes: %f\n" , habitantes);
+    printf("Área da cidade: %f km\n" , area);
+    printf("Código da carta: %f/n" , codigo);
+    printf("O PIB da cidade é: %f\n bilhoes de reais" , pib);
+    printf("numero de pontos turisticos: %d\n" , numero);
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
